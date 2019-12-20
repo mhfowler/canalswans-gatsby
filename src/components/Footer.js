abff9d479e8315a494d156686871076ceabe9beb
Max Fowler <maxhfowler@gmail.com>
2019-12-20 14:09:20 -0500

    Canal swans

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,7 @@ import SLink from "./SLink";
 const Footer = ({reversed}) => {
   return (
     <footer className="footer">
-      <div className="footer-links-wrapper">
-        <div>
-          <SLink style={{"color":"black"}} className="hlink" to='/'>Canal Swans</SLink>
-        </div>
-        <div>
-          {/*<SLink className="hlink" to='/about'>about</SLink>*/}
-          {/*<SLink className="hlink" to='/rss'>rss</SLink>*/}
-          {/*<SLink className="hlink" to='tinyletter.com/newsletter'>newsletter</SLink>*/}
-          {/*<SLink className="hlink" to='/essays'>essays</SLink>*/}
-          {/*<SLink className="hlink" to='/zines'>zines</SLink>*/}
-        </div>
-      </div>
+      <SLink style={{"color":"black"}} className="hlink" to='/'>Canal Swans</SLink>
     </footer>
   )
 }
